f87cac7c3ae8dea8fc82403037a90df85d654e90
rajibghosal2016 <rajibghosal2016@gmail.com>
2022-06-30 07:06:35 +0530

    Commented out all console.log()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,11 @@ export class AppComponent implements OnDestroy {
   title = 'InsideFrame';
 
   ngOnDestroy(): void {
-    console.log('inside iframe is geting destroyed');
+    //console.log('inside iframe is geting destroyed');
   }
 
   @HostListener('window:message', ['$event'])
   onMessage(e: any) {
-    console.log(e);
     if (e["data"] == "close")
       this.closeApp();
   }
